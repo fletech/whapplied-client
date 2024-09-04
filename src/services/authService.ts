06f78f12418385a28d662bedb664e47cafb5f591
fletech <facundogarcia.ar@gmail.com>
2024-09-04 11:07:12 +0200

    header

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -22,13 +22,8 @@ export class AuthService implements IAuthService {
           method: "GET",
           credentials: "include",
           headers: {
-            "Access-Control-Allow-Origin":
-              "https://whapplied-client.vercel.app",
-            "Access-Control-Allow-Credentials": "true",
-            "Access-Control-Allow-Methods": "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-            "Access-Control-Allow-Headers":
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
             "Content-Type": "application/json",
+            Accept: "application/json",
           },
         }
       );
@@ -69,12 +64,6 @@ export class AuthService implements IAuthService {
           method: "GET",
           credentials: "include",
           headers: {
-            "Access-Control-Allow-Origin":
-              "https://whapplied-client.vercel.app",
-            "Access-Control-Allow-Credentials": "true",
-            "Access-Control-Allow-Methods": "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-            "Access-Control-Allow-Headers":
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
             "Content-Type": "application/json",
             Accept: "application/json",
           },
