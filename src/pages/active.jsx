b05042228041bae204905146308d4ee51069ad88
fletech <facundogarcia.ar@gmail.com>
2024-09-06 23:50:38 +0200

    get rid console.log

--- src/pages/active.jsx
+++ src/pages/active.jsx
@@ -2,7 +2,6 @@ import React, { useContext, useEffect, useState } from "react";
 import TableContainer from "../components/TableContainer";
 
 import { TableContext } from "../context/tableContext";
-// const Dashboard = React.lazy(() => import('../components/Dashboard'));
 
 const ActivePage = () => {
   const { setPageFilter } = useContext(TableContext);
