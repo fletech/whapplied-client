4a447848ec66539b355b2a93c815d15b3c10d23b
fletech <facundogarcia.ar@gmail.com>
2024-09-07 00:07:16 +0200

    purge

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -42,9 +42,6 @@ export class AuthService implements IAuthService {
         },
       });
 
-      console.log("Response status:", response.status);
-      console.log("Response headers:", response.headers);
-
       if (!response.ok) {
         console.log("Auth check failed");
         return null;
